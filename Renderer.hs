b2692cdcb198dc42eecb97e3a97bd3693fb2a28a
Luka Macharashvili <macharashvililuka12@gmail.com>
2024-01-12 01:51:15 +0400

    ...

--- Renderer.hs
+++
@@ -1,6 +0,0 @@
-module Renderer (render) where
-
-import Parser
-
-render :: Maybe HtmlElement
-render = Parser.parseHtml "<section dd=\"mmmm\"><a href=\"dawdawda\">daaaaaa</a><img src=\"dawdawda\" alt=\"alt data\" /></section>" -- TEMP
